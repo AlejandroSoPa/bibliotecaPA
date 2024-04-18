ed0e7aa4862a2824eeba36baf45dc7520d7658e3
Alejandro <95918617+AlejandroSoPa@users.noreply.github.com>
2024-04-18 20:17:12 +0200

    initial commit django

--- django/bibliotecaPa/bibliotecaPa/settings.py
+++ django/bibliotecaPa/bibliotecaPa/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%8!fm-nlez2xdw&-6r8xu(bs15f4f&1z^om_a$1d_fngn4&zdb'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
